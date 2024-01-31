9a70842f5a269411b39b4a607c0135f8478b5373
Historia <1617174280@qq.com>
2024-02-01 02:53:13 +0800

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+void solustion()
+{
+    cout << "Hello,NJU!" << endl;
+    cout << "Hello,world!" << endl;
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solustion();
+    return 0;
+}
