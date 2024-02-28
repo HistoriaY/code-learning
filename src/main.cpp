796fd0ed28c2a4d80ed0d62a7260dc3a65e9fb26
Historia <1617174280@qq.com>
2024-02-29 01:22:05 +0800

    update included head files

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,16 @@
 #include <unordered_set>
 #include <set>
 #include <queue>
+#include <string>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
-
-void solustion()
-{
-    cout << "Hello,NJU!" << endl;
-    cout << "Hello,world!" << endl;
-    return;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    solustion();
     return 0;
 }
