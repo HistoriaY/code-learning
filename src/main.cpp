e5952be05fd8745dda6dce1e6eb561949926b7fe
HistoriaY <1617174280@qq.com>
2024-05-01 17:12:07 +0800

    inputs file & freopen

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <unordered_map>
 #include <map>
@@ -11,10 +12,21 @@
 
 using namespace std;
 
+const int maxn = 1e5 + 7;
+const int maxm = 1e5 + 7;
+int dp[maxn][maxm];
+int n, m;
+
+void solution()
+{
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    freopen("../inputs/input.txt", "r", stdin);
+    solution();
     return 0;
 }
